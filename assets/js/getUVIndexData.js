0894f4aee43b7ea2757fd6589251bc3b767e67a1
bbrintle <bbrintle@gmail.com>
2020-09-15 16:00:58 -0700

    removed some unneeded files

--- assets/js/getUVIndexData.js
+++
@@ -1,9 +0,0 @@
-function getUVIndexData(uvURL){
-    $.ajax({
-        url: uvURL,
-        method: "GET"
-    }).then(function(response){ 
-        console.log(response.value)
-        return response.value;
-    });
-}
